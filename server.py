cb5dd5b569671255c9bd6619631b237f0b219cc7
Usama Kenway <usamakenway@gmail.com>
2023-07-20 18:27:09 +0500

    feat : server.py to run gradio and uvicorn automatically

--- server.py
+++ server.py
@@ -2,6 +2,12 @@ import gradio as gr
 import random
 import time
 from gradio.chatbot import ChatBot
+import gradio as gr
+import requests
+from fastapi import FastAPI
+from fastapi.routing import APIRouter
+from LLMApp.core.event_handler import start_app_handler
+from LLMApp.api.api import predict
 
 
 class ChatBotApp:
@@ -14,7 +20,15 @@ class ChatBotApp:
         return "", chatbot
 
 
-if __name__ == "__main__":
+def main():
+    app = FastAPI()
+    app.add_event_handler("startup", start_app_handler(
+        app, "usamakenway/Wizard-Vicuna-13B-Uncensored-AutoGPTQ"))
+
+    router = APIRouter()
+    router.include_router(predict)
+    app.include_router(router)
+
     bot_app = ChatBotApp()
 
     with gr.Blocks() as demo:
@@ -27,3 +41,13 @@ if __name__ == "__main__":
         msg.submit(bot_app.run, [msg, chatbot], [msg, chatbot])
 
     demo.launch(share=True)
+
+
+if __name__ == "__main__":
+    import threading
+    import uvicorn
+
+    server_thread = threading.Thread(target=lambda: uvicorn.run("main:app", host="0.0.0.0", port=8080, log_level="info"))
+    server_thread.start()
+
+    main()
