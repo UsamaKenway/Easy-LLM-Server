6ba903c90347a401c310b1fd4c68fd52e1a22eca
Usama Kenway <usamakenway@gmail.com>
2023-07-23 21:58:43 +0500

    refac| server.py

--- server.py
+++ server.py
@@ -1,9 +1,7 @@
 from gradio_.chatbot import ChatBot
 import gradio as gr
-from fastapi import FastAPI
-from LLMApp.core.event_handler import start_app_handler
-from LLMApp.api.api import router
-import asyncio
+import threading
+import uvicorn
 
 
 class ChatBotApp:
@@ -16,38 +14,9 @@ class ChatBotApp:
         return "", chatbot
 
 
-def main():
-    app = FastAPI()
-    app.add_event_handler("startup", start_app_handler(
-        app, "usamakenway/Wizard-Vicuna-7B-Uncensored-SuperHOT-8K-AutoGPTQ"))
-
-    app.include_router(router)
-
-    # Use asyncio event loop to wait for FastAPI server to start
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(start_fastapi_server(app))
-
-
-async def start_fastapi_server(app):
-    import uvicorn
-    config = uvicorn.Config(app, host="0.0.0.0", port=8080, log_level="info")
-    server = uvicorn.Server(config)
-
-    # Start the FastAPI server asynchronously
-    asyncio.ensure_future(server.serve())
-
-    # Wait for the FastAPI server to be fully running
-    while True:
-        if server.started:
-            break
-        await asyncio.sleep(0.1)
-
-    # FastAPI server is running, now start Gradio application
-    start_gradio_app()
-
-
-def start_gradio_app():
+def run_gradio_server():
     bot_app = ChatBotApp()
+
     with gr.Blocks() as demo:
         no = gr.Label("LLM WebUI Gradio Server")
         chatbot = gr.Chatbot()
@@ -56,11 +25,19 @@ def start_gradio_app():
         clear = gr.ClearButton([msg, chatbot])
 
         msg.submit(bot_app.run, [msg, chatbot], [msg, chatbot])
-    demo.launch(share=True)
+
+    demo.launch(share=True, debug=True)
+
+
+def run_fastapi_server():
+    uvicorn.run("LLMApp.main:app", host="0.0.0.0", port=8080, log_level="info")
 
 
 if __name__ == "__main__":
-    # Run the main function to start both FastAPI and Gradio
-    main()
 
+    # Start FastAPI server in a separate thread
+    fastapi_server_thread = threading.Thread(target=run_fastapi_server)
+    fastapi_server_thread.start()
 
+    # Start Gradio server in the main thread
+    run_gradio_server()
